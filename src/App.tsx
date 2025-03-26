aefdac75d1a7060eeaddd8e051a63dafd49a4680
ErikssonJoakim <75730728+ErikssonJoakim@users.noreply.github.com>
2025-03-26 20:57:45 +0100

    feat: random banner

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react"
 import logo from "./logo.svg"
 import "./App.css"
 import Cntr from "./components/Cntr"
+import Bnr from "./components/Bnr"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Bnr />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
